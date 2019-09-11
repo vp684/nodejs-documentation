0909077a9b82762acf244020a89b1b40300bd5e2
vp684 <Viraj84@gmail.com>
2019-09-10 22:38:34 -0400

    first commit

---
+++ src/script.js
@@ -0,0 +1,37 @@
+/*
+script for testing https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js
+*/
+document.addEventListener("DOMContentLoaded", function() {
+  hljs.initHighlightingOnLoad();   
+  
+});
+                          
+
+window.addEventListener('resize', ()=>{
+  toggleMenu(true)// 
+});
+
+function toggleMenu(event) {
+  //only hide links if on mobile.
+  var x = document.getElementById("links");
+  if(event){
+    //event on resize so keep closed until manually opened
+    // must be less than media min-width
+    if(window.innerWidth < 801){
+      if (x.style.display !== "none") x.style.display = "none";
+    }else x.style.display = "block";     
+    
+  }else{//not an event so must be click
+    
+    if(window.innerWidth < 801){
+      if (x.style.display === "block") x.style.display = "none"
+      else x.style.display = "block";
+    }
+    
+  }
+  
+    
+  
+}
+
+
